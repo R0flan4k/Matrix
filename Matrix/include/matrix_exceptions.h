b53fd2d2e4e150610c40d424eaefdf441ff0ee61
R0flan4k <klexak05@gmail.com>
2025-03-17 21:23:04 +0300

    Small changes.

--- Matrix/include/matrix_exceptions.h
+++ Matrix/include/matrix_exceptions.h
@@ -7,14 +7,15 @@ namespace MatrExcepts {
 class matrix_error : public std::runtime_error {
 public:
     matrix_error(const std::string &what_arg) : std::runtime_error(what_arg) {}
+    virtual ~matrix_error() = default;
 };
 
-class wrong_init_list : public matrix_error {
+class wrong_init_list final : public matrix_error {
 public:
     wrong_init_list(const std::string &what_arg) : matrix_error(what_arg) {}
 };
 
-class no_det : public matrix_error {
+class no_det final : public matrix_error {
 public:
     no_det(const std::string &what_arg) : matrix_error(what_arg) {}
 };
